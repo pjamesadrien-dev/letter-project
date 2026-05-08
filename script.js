0023b904299e9df93eb564a81f61aa3ad5e9b521
mist <pjamesadrien@gmail.com>
2026-05-08 13:14:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = 'I guess there’s no point in waiting for your answer anymore,\nbut thank you. I still appreciate the short time and effort you gave me.\nStay strong, bye-bye, Miss Avoidant.\n\nI honestly wished things turned out differently, but maybe some people are only meant to pass through our lives for a short while.\nStill, I’m grateful I got to know you, even for a little bit. Take care of yourself always.`;
+const message = 'I guess there’s no point in waiting for your answer anymore,\nbut thank you. I still appreciate the short time and effort you gave me.\nStay strong, bye-bye, Miss Avoidant.\n\nI honestly wished things turned out differently, but maybe some people are only meant to pass through our lives for a short while.\nStill, I’m grateful I got to know you, even for a little bit. Take care of yourself always.';
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -7,7 +7,9 @@ function showLetter() {
   setTimeout(() => {
     const letterBox = document.getElementById("letterBox");
     const typedText = document.getElementById("typedText");
+
     letterBox.style.display = "block";
+
     let i = 0;
 
     function typeWriter() {
