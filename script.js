38af38b7ae549c64591870e0fc2d4ae43990e8c8
mist <pjamesadrien@gmail.com>
2026-05-08 12:56:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = I guess there’s no point in waiting for your answer anymore,
+const message = 'I guess there’s no point in waiting for your answer anymore,
 but thank you. I still appreciate the short time and effort you gave me. Stay strong, bye-bye, Miss Avoidant.
 
 We both tried. We both cared. But the universe just never gave us enough space to become “us.”
